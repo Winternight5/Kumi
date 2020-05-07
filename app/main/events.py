ee45add726c74cf92d09c2b2a8c2cf058713876f
@Winternight5 <tai.p.huynh@gmail.com>
2020-05-07 08:32:04 -0700

    fixing maximum call stack

--- app/main/events.py
+++ app/main/events.py
@@ -85,7 +85,7 @@ def update_stats():
         'clients': [int(i) for i in clients]
     }, broadcast=True)
 
-@socketio.on('disconnect', namespace='/')
+@socketio.on('disconnected', namespace='/')
 def disconnect():
     '''
     User Disconnected
